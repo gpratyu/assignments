56bc506160d7d39c03a1fe1e5116f8e80c094c5c
gpratyu <pratyushagovindaraju@gmail.com>
2020-01-12 15:35:46 -0500

    Added print message

--- calculator.py
+++ calculator.py
@@ -9,3 +9,20 @@ def mult(x,y):
 
 def div(x,y):
 	return x / y
+
+print("I'm going use the calculator functions to add 5 and 6")
+a = add(5,6)
+print(a)
+
+print("I'm going use the calculator functions to calculate the difference between 5 and 6")
+b = diff(5,6)
+print(b)
+
+print("I'm going use the calculator functions to multiply 5 by 6")
+c = mult(5,6)
+print(c)
+
+print("I'm going use the calculator functions to divide 5 by 6")
+d = div(5,6)
+print(d)
+
