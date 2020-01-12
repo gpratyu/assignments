60aa2fcb0a749e0af7f27625a185631256c52de8
gpratyu <pratyushagovindaraju@gmail.com>
2020-01-12 15:58:34 -0500

    Adding bonus questions

---
+++ calculator.py
@@ -0,0 +1,51 @@
+def add(x,y):
+	return x + y
+
+def diff(x,y):
+	return x - y
+
+def mult(x,y):
+	return x * y
+
+def div(x,y):
+	return x / y
+
+def square(x):
+	return x * x
+
+def cube(x):
+	return x ** 3
+
+def square_n_times(number,n):
+	return (number ** n)
+
+
+print("I'm going to use the calculator functions to add 5 and 6")
+a = add(5,6)
+print(a)
+
+print("I'm going to use the calculator functions to calculate the difference between 5 and 6")
+b = diff(5,6)
+print(b)
+
+print("I'm going to use the calculator functions to multiply 5 by 6")
+c = mult(5,6)
+print(c)
+
+print("I'm going to use the calculator functions to divide 5 by 6")
+d = div(5,6)
+print(d)
+
+print("I'm going to use the calculator functions to sqaure 5")
+e = square(5)
+print(e)
+
+print("I'm going to use the calculator functions to cube 5")
+f = cube(5)
+print(f)
+
+
+print("I'm going to use the calculator functions to square 2, 5 times")
+g = square_n_times(2,5)
+print(g)
+
