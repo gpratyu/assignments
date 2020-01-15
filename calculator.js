7bd991f6140a9975741daa47e9290331a8bad023
gpratyu <pratyushagovindaraju@gmail.com>
2020-01-14 22:05:24 -0500

    add print command

--- calculator.js
+++ calculator.js
@@ -16,3 +16,18 @@ function divide(a,b) {
     return a / b
 }
 
+// This part of the code then calls that function and assigns the variable x to its return value
+console.log("I'm going use the calculator functions to add 5 and 6")
+var x = add(5,6)
+
+console.log("I'm going use the calculator functions to subtract 5 and 6")
+var x = subtract(5,6)
+
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+
+console.log("I'm going use the calculator functions to divide 5 and 6")
+var x = divide(5,6)
+
+// This prints x to the terminal for humans to read
+console.log(x)
